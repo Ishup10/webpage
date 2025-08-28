c783f9f9b399ed0484885fc1fdd6f9c67496ee4d
Ishup Bhujel <ishupbhujel168@gmail.com>
2025-08-28 12:16:10 +0545

    update page

--- src/Pages/Notfound.jsx
+++ src/Pages/Notfound.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const Notfound = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-gray-800">
-        <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-600 rounded-full animate-spin"></div>
-      <h1 className="text-3xl font-bold mb-6">🚫 Webpage Not Found</h1>
+        <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-600 rounded-full animate-spin mb-4"></div>
+      <h1 className="text-3xl font-bold "> Website is under maintainenace </h1>
+      <span className='text-3xl font-bold'>please come back after time</span>
       
     </div>
     
