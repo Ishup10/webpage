a24b0d4bde9a58191c07ceeb083681a226730cc8
Ishup Bhujel <ishupbhujel168@gmail.com>
2025-08-28 13:00:07 +0545

    update not found

--- src/Pages/Notfound.jsx
+++ src/Pages/Notfound.jsx
@@ -4,8 +4,8 @@ const Notfound = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100 text-gray-800">
         <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-600 rounded-full animate-spin mb-4"></div>
-      <h1 className="text-3xl font-bold "> Website is under maintainenace </h1>
-      <span className='text-3xl font-bold'>please come back after time</span>
+      <h1 className="text-3xl font-bold  "> Website is under maintainenace </h1>
+      <span className='text-3xl font-bold '>Please come back after some time</span>
       
     </div>
     
